80ea03202e2236ba50bdebe56ba23b82cc8e5f5c
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-01-06 16:16:42 +0900

    Support keymap

--- imshow/_generators.py
+++ imshow/_generators.py
@@ -1,5 +1,5 @@
-from typing import Any
 import types
+from typing import Any
 
 
 class CachedGenerator:
--- imshow/_imshow.py
+++ imshow/_imshow.py
@@ -7,32 +7,33 @@ from typing import Optional
 import numpy as np
 import pyglet
 
-from imshow import _pyglet
 from imshow import _generators
+from imshow import _pyglet
 
 
 def imshow(
     items: Any,
     *,
+    keymap: Optional[dict] = None,
     get_image_from_item: Optional[Callable] = None,
     get_caption_from_item: Optional[Callable] = None,
 ) -> None:
     if not isinstance(items, (list, types.GeneratorType)):
         items = [items]
+    if keymap is None:
+        keymap = {}
     if get_image_from_item is None:
-        get_image_from_item = lambda item: item
+
+        def get_image_from_item(item):
+            return item
+
     if get_caption_from_item is None:
-        get_caption_from_item = lambda item: str(item)
+
+        def get_caption_from_item(item):
+            return str(item)
 
     items = _generators.CachedGenerator(iter(items))
-
-    image: np.ndarray = (
-        get_image_from_item(next(items)) if get_image_from_item else items
-    )
-    if not isinstance(image, np.ndarray):
-        raise TypeError(
-            f"get_image_from_item must return numpy.ndarray, but got {type(image)}"
-        )
+    image: np.ndarray = get_image_from_item(next(items))
 
     aspect_ratio: float = image.shape[1] / image.shape[0]  # width / height
     window = _pyglet.initialize_window(aspect_ratio=aspect_ratio)
@@ -47,49 +48,70 @@ def imshow(
         window.clear()
         sprite.draw()
 
-    def usage():
-        print("Usage: ", file=sys.stderr)
-        print("  h: show help", file=sys.stderr)
-        print("  q: close window", file=sys.stderr)
-        print("  n: next image", file=sys.stderr)
-        print("  p: previous image", file=sys.stderr)
+    state = types.SimpleNamespace(items=items, index=0)
 
-    usage()
+    def show_help(state):
+        usage()
 
-    state = types.SimpleNamespace(index=0)
+    def close_window(state):
+        window.close()
 
-    def update_with_item(item):
+    def update(item):
         window.set_caption(get_caption_from_item(item))
         sprite.image = _pyglet.convert_to_imagedata(get_image_from_item(item))
         _pyglet.centerize_sprite_in_window(sprite, window)
 
+    def next_image(state):
+        try:
+            try:
+                item = state.items[state.index + 1]
+            except IndexError:
+                item = next(state.items)
+            update(item)
+            state.index += 1
+        except StopIteration:
+            pass
+
+    def previous_image(state):
+        try:
+            if state.index > 0:
+                item = state.items[state.index - 1]
+            else:
+                raise IndexError
+            update(item)
+            state.index -= 1
+        except IndexError:
+            pass
+
+    def usage():
+        print("Usage: ", file=sys.stderr)
+        for (symbol, modifiers), action in keymap.items():
+            symbol_string = pyglet.window.key.symbol_string(symbol).lower()
+            modifiers_string = pyglet.window.key.modifiers_string(modifiers)
+            if modifiers_string:
+                key = f"{modifiers_string}+{symbol_string}"
+            else:
+                key = symbol_string
+            print(
+                f"  {key}: {' '.join(action.__name__.split('_'))}",
+                file=sys.stderr,
+            )
+
+    DEFAULT_KEYMAP = {
+        (pyglet.window.key.H, 0): show_help,
+        (pyglet.window.key.Q, 0): close_window,
+        (pyglet.window.key.N, 0): next_image,
+        (pyglet.window.key.P, 0): previous_image,
+    }
+    for key, action in DEFAULT_KEYMAP.items():
+        if key not in keymap:
+            keymap[key] = action
+
+    usage()
+
     @window.event()
     def on_key_press(symbol, modifiers):
-        if symbol == pyglet.window.key.Q:
-            window.close()
-        elif symbol == pyglet.window.key.N:
-            try:
-                try:
-                    item = items[state.index + 1]
-                except IndexError:
-                    item = next(items)
-                update_with_item(item)
-                state.index += 1
-            except StopIteration:
-                pass
-        elif symbol == pyglet.window.key.P:
-            try:
-                if state.index > 0:
-                    item = items[state.index - 1]
-                else:
-                    raise IndexError
-                update_with_item(item)
-                state.index -= 1
-            except IndexError:
-                pass
-        elif symbol == pyglet.window.key.H:
-            usage()
-        else:
-            print("Press 'h' to show help", file=sys.stderr)
+        if (symbol, modifiers) in keymap:
+            keymap[(symbol, modifiers)](state=state)
 
     pyglet.app.run()
