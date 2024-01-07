62dfbc5717f9a05641ac3f1c8a5591910a428127
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-01-07 14:14:40 +0900

    Use typing.List instead of list

--- imshow/plugins/tile.py
+++ imshow/plugins/tile.py
@@ -1,5 +1,6 @@
 import itertools
 import os
+from typing import List
 
 import imgviz
 import numpy as np
@@ -53,9 +54,9 @@ class Plugin(base.Plugin):
 
     row: int
     col: int
-    padding_color: list[int]
+    padding_color: List[int]
     border_width: int
-    border_color: list[int]
+    border_color: List[int]
 
     def __init__(self, args):
         super().__init__(args=args)
